741bd96aa81bfc5e6b0b72217d0ace8568506180
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-08-11 04:38:40 -0400

    fix: fixing spacing in calculadora.ts

--- src/hvm/Calculadora.ts
+++ src/hvm/Calculadora.ts
@@ -1,46 +1,46 @@
 export default class Calculadora {
     
-    private acumulador = 0;
+  private acumulador = 0;
+
+  public soma(valor:number) {
+    
+    this.acumular(this.acumulador + valor)
+
+  }
+
+  public subtraia(valor:number) {
+
+    this.acumular(this.acumulador - valor)
+
+  }
+
+  public multiplicar(valor:number) {
+
+    this.acumular(this.acumulador * valor);
+
+  }
+
+  public divida(valor:number) {
+
+    this.acumular(this.acumulador / valor);
+
+  }
+
+  public acumular(valor:number) {
+
+    valor = parseInt(valor.toString())
   
-    public soma(valor:number) {
-      
-      this.acumular(this.acumulador + valor)
-
-    }
+    if(valor < -99 || valor > 999) 
+      throw new Error(`Erro calculadora ${valor}. Apenas valores entre 0-999 são aceitos.`);
+        
+    this.acumulador = valor;
   
-    public subtraia(valor:number) {
-
-      this.acumular(this.acumulador - valor)
-
-    }
+  }
   
-    public multiplicar(valor:number) {
-
-      this.acumular(this.acumulador * valor);
-
-    }
+  public getAcumulador() {
   
-    public divida(valor:number) {
-
-      this.acumular(this.acumulador / valor);
-
-    }
-
-    public acumular(valor:number) {
-
-      valor = parseInt(valor.toString())
-    
-      if(valor < -99 || valor > 999) 
-        throw new Error(`Erro calculadora ${valor}. Apenas valores entre 0-999 são aceitos.`);
-          
-      this.acumulador = valor;
-    
-    }
-    
-    public getAcumulador() {
-    
-      return this.acumulador;
-    
-    }
+    return this.acumulador;
+  
+  }
 
 }
