f84793194db4217c0bc667ac3fb5e07087dad16d
WilkerSebastian <wilkerafonso2005@gmail.com>
2024-08-11 04:39:25 -0400

    fix: fixing spacing in gaveteiro.ts

--- src/hvm/Gaveteiro.ts
+++ src/hvm/Gaveteiro.ts
@@ -4,85 +4,84 @@ import PortaCartoes from "./PortaCartoes";
 
 export default class Gaveteiro {
 
-    private ultimoRestrito:number;
-    private gavetas:string[];
+  private ultimoRestrito:number;
+  private gavetas:string[];
 
-    constructor(max_gavetas:number = 100) {
+  constructor(max_gavetas:number = 100) {
 
-      this.gavetas = new Array<string>(max_gavetas)
-      this.ultimoRestrito = 0
+    this.gavetas = new Array<string>(max_gavetas)
+    this.ultimoRestrito = 0
 
-    }
+  }
 
-    public getGavetas() {
+  public getGavetas() {
 
-      return this.gavetas
+    return this.gavetas
 
-    }
+  }
 
-    public async carga(portaCartao:PortaCartoes, delay:number, call:(state:HVMState) => void){
+  public async carga(portaCartao:PortaCartoes, delay:number, call:(state:HVMState) => void){
+    
+    let index = 0
+    let final = false;
+
+    while(!final) {
+
+      if (delay > 0)
+        await sleep(delay);
       
-      let index = 0
-      let final = false;
+      const cartao = portaCartao.lerCartao()
 
-      while(!final) {
+      call("CARGA")
+      
+      if (cartao) {
 
-        if (delay > 0)
-          await sleep(delay);
-        
-        const cartao = portaCartao.lerCartao()
+        this.registrar(index, cartao)
 
         call("CARGA")
         
-        if (cartao) {
+        final = cartao == "000"
 
-          this.registrar(index, cartao)
+      } else
+        throw new Error("Falha na carga do porta cartões para gaveteiro");
 
-          call("CARGA")
-          
-          final = cartao == "000"
-
-        } else
-          throw new Error("Falha na carga do porta cartões para gaveteiro");
-
-        index++
-
-      }
-
-      this.ultimoRestrito = index
-        
-    }
-  
-    public registrar(endereco: number, valor: string) {
-
-      if(endereco + 1 > this.gavetas.length)
-        throw new Error(`Erro de sobrecarga de gavetas, limite de ${this.gavetas.length} registros`);
-      else if(endereco < 0)
-        throw new Error(`Erro de estouro negativo de gavetas, as gabetas vão de 0 a ${this.gavetas.length} registros`);
-
-      if((endereco < this.ultimoRestrito) && this.ultimoRestrito > 0){
-          const conteudo = this.ler(endereco);
-          throw new Error(`Erro tentativa de sobrescrita de gaveta que armazena código fonte conteúdo da gaveta(${endereco}): ${conteudo}`);
-      }
-
-      const numeric_value = parseInt(valor)
-
-      if (numeric_value < 0 || numeric_value > 999)
-        throw new Error(`Valor invalido de escrita na gaveta [${endereco}]:${numeric_value}`)
-
-      this.gavetas[endereco] = valor
+      index++
 
     }
-  
-    public ler(endereco: number){
 
-      if (endereco < 0 || endereco > this.gavetas.length || !this.gavetas[endereco])   
-        throw new Error(`Erro na leitura do gaveteiro no endereço ${endereco}, tentativa de leitura em endereço inexistente`);
+    this.ultimoRestrito = index
+      
+  }
 
+  public registrar(endereco: number, valor: string) {
 
-      return this.gavetas[endereco];
+    if(endereco + 1 > this.gavetas.length)
+      throw new Error(`Erro de sobrecarga de gavetas, limite de ${this.gavetas.length} registros`);
+    else if(endereco < 0)
+      throw new Error(`Erro de estouro negativo de gavetas, as gabetas vão de 0 a ${this.gavetas.length} registros`);
 
+    if((endereco < this.ultimoRestrito) && this.ultimoRestrito > 0){
+        const conteudo = this.ler(endereco);
+        throw new Error(`Erro tentativa de sobrescrita de gaveta que armazena código fonte conteúdo da gaveta(${endereco}): ${conteudo}`);
     }
+
+    const numeric_value = parseInt(valor)
+
+    if (numeric_value < 0 || numeric_value > 999)
+      throw new Error(`Valor invalido de escrita na gaveta [${endereco}]:${numeric_value}`)
+
+    this.gavetas[endereco] = valor
 
   }
-  
+
+  public ler(endereco: number){
+
+    if (endereco < 0 || endereco > this.gavetas.length || !this.gavetas[endereco])   
+      throw new Error(`Erro na leitura do gaveteiro no endereço ${endereco}, tentativa de leitura em endereço inexistente`);
+
+
+    return this.gavetas[endereco];
+
+  }
+
+}
