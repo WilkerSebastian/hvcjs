83c32d1b7874109097248528be5e13080744e1cb
LuizFXavier <zap422238@gmail.com>
2024-08-29 20:33:08 -0400

    feat: Debugger

---
+++ src/hvm/Debugger.ts
@@ -0,0 +1,29 @@
+import DAO from "./DAO";
+import HVM from "./HVM";
+
+export default class Debugger{
+    private estados:DAO[] = [];
+    nStages:number = 0;
+
+    public loadLastStage(hvm:HVM){
+
+        if(this.nStages == 0)
+            return;
+        
+        const estado = this.estados.pop()
+
+        hvm.calculadora.acumular(estado!.acumulador)
+        hvm.folhaDeSaida.imprimir(estado!.folhaDeSaida)
+        
+        hvm.gaveteiro.setGavetas(estado!.gaveteiro)
+        hvm.epi.registrar(estado!.valorEpi)
+
+        this.nStages -= 1;
+    }
+    public storeStage(hvm:HVM, estadoAtual:number){
+
+        this.estados.push(new DAO(hvm, estadoAtual))
+        this.nStages += 1;
+    }
+    
+}
