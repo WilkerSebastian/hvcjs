f49167a0fb477dee38a2df8cae61249ffda8c87f
LuizFXavier <zap422238@gmail.com>
2024-09-02 02:22:10 -0400

    Mudança na ordem de execução e salvamento de estado

--- src/hvm/Debugger.ts
+++ src/hvm/Debugger.ts
@@ -3,40 +3,46 @@ import DAO from "./DAO";
 import HVM from "./HVM";
 
 export default class Debugger{
-    private estados:DAO[] = [];
+    public estados:DAO[] = [];
     nStages:number = 0;
-    private state:DebuggerState = "EXECUTANDO"
+    private state:DebuggerState = "RODANDO"
     
-    public setState(state:DebuggerState){
+    public async setState(state:DebuggerState){
         this.state = state
     }
     public getState(){
         return this.state;
     }
 
-    public loadLastStage(hvm:HVM):number{
+    public async loadLastStage(hvm:HVM):Promise<number>{
 
         if(this.nStages == 0)
             return -1;
         
         const estado = this.estados.pop()
-
+        console.log("volta para:", estado);
+        
         hvm.calculadora.acumular(estado!.acumulador)
         hvm.folhaDeSaida.imprimir(estado!.folhaDeSaida)
         
         hvm.gaveteiro.setGavetas(estado!.gaveteiro)
+        // console.log("gavetosas:",hvm.gaveteiro.getGavetas());
+        
         hvm.epi.registrar(estado!.valorEpi)
 
         this.nStages -= 1;
 
         return estado!.enderecoAtual;
     }
-    public nextStage(hvm:HVM){
+    public async nextStage(hvm:HVM){
 
         const enderecoAtual = hvm.epi.lerRegistro()
-        hvm.executarPasso()
         this.estados.push(new DAO(hvm, enderecoAtual))
         this.nStages += 1;
+        await hvm.executarPasso()
+
+        console.log(this.estados[this.nStages -1]);
+        
     }
     
 }
