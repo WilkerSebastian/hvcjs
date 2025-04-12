9b740622bf4d90cfd8ef0f2a686c792d305e6823
LuizFXavier <zap422238@gmail.com>
2025-04-12 16:03:46 -0400

    feat: Registros anteriores da folha de saída

--- src/hvm/FolhaDeSaida.ts
+++ src/hvm/FolhaDeSaida.ts
@@ -2,12 +2,12 @@ import { Internalization } from "../utils/Internalization";
 
 export default class FolhaDeSaida {
 
-    private text:string = "";
+    private text:string[] = [];
 
     public saida?:(out:string) => void
     
     public imprimir(texto: string) {
-        this.text = texto;
+        this.text.push(texto);
         if (this.saida)  {
 
             this.saida(texto)
@@ -18,8 +18,15 @@ export default class FolhaDeSaida {
         throw new Error(Internalization.getInstance().translate("not_implemented_out"))
 
     }
+    public setText(saidas:string[]){
+        this.text = saidas;
+    }
 
-    getText(){
+    public getText(id?:number):string[] | string{
+        
+        if(id != undefined)
+            return this.text[id];
+
         return this.text;
     }
 }
